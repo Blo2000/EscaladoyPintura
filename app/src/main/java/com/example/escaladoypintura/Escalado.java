ae0b4f0a1dbe7e2e5223c6a0a3f2f5c39a40660c
blo2000 <pablo.hernandez.ortiz.12.8.2000@gmail.com>
2024-05-19 21:05:59 +0200

    Errores de emulacion solucionados

--- app/src/main/java/com/example/escaladoypintura/Escalado.java
+++ app/src/main/java/com/example/escaladoypintura/Escalado.java
@@ -1,27 +1,47 @@
 package com.example.escaladoypintura;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class Escalado extends AppCompatActivity {
     Number num;
+    private Spinner desplegable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.escalado);
 
-        // opciones del despelgable
-        String[] opciones = {"1/72", "1/76", "1/48", "1/35", "1/32"};
+        desplegable = (Spinner) findViewById(R.id.desplegable);
+        // opciones del despelgable+
+        ArrayList<String> opciones = new ArrayList<>();
+        opciones.add("1/72");
+        opciones.add("1/76");
+        opciones.add("1/48");
+        opciones.add("1/35");
+        opciones.add("1/32");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, opciones);
-        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.desplegable);
-        autoCompleteTextView.setAdapter(adapter);
+        ArrayAdapter adp = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, opciones);
 
+        desplegable.setAdapter(adp);
+        desplegable.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String op  = (String) desplegable.getAdapter().getItem(position);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
 
 
     }
