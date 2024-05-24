2a103174c6e7588016bbbac6a0029fd72f0a3765
blo2000 <pablo.hernandez.ortiz.12.8.2000@gmail.com>
2024-05-24 18:57:50 +0200

    Escaldado hecho

--- app/src/main/java/com/example/escaladoypintura/Escalado.java
+++ app/src/main/java/com/example/escaladoypintura/Escalado.java
@@ -5,7 +5,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,12 +16,16 @@ import java.util.ArrayList;
 public class Escalado extends AppCompatActivity {
     Number num;
     private Spinner desplegable;
+    TextView resultado;
+    EditText datos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.escalado);
-
+        resultado = findViewById(R.id.resultado);
         desplegable = (Spinner) findViewById(R.id.desplegable);
+        datos = findViewById(R.id.datos);
+
         // opciones del despelgable+
         ArrayList<String> opciones = new ArrayList<>();
         opciones.add("1/72");
@@ -34,7 +40,36 @@ public class Escalado extends AppCompatActivity {
         desplegable.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String op  = (String) desplegable.getAdapter().getItem(position);
+                String op = (String) desplegable.getAdapter().getItem(position);
+                String datosStr = datos.getText().toString();
+                if (!datosStr.isEmpty()) {
+                    try {
+                        double dat = Double.parseDouble(datosStr);
+                        double resultadoEscala = 0;
+
+                        switch (op) {
+                            case "1/72":
+                                resultadoEscala = escalar(dat, 72);
+                                break;
+                            case "1/76":
+                                resultadoEscala = escalar(dat, 76);
+                                break;
+                            case "1/48":
+                                resultadoEscala = escalar(dat, 48);
+                                break;
+                            case "1/35":
+                                resultadoEscala = escalar(dat, 35);
+                                break;
+                            case "1/32":
+                                resultadoEscala = escalar(dat, 32);
+                                break;
+                        }
+
+                        resultado.setText(String.format(String.valueOf(resultadoEscala)));
+                    } catch (NumberFormatException e) {
+                        datos.setError("Numero no valido");
+                    }
+                }
             }
 
             @Override
@@ -42,7 +77,8 @@ public class Escalado extends AppCompatActivity {
 
             }
         });
-
-
+    }
+    public static double escalar(double medida, double escala){
+        return medida/escala;
     }
 }
