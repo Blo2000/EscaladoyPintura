f03793f010f4ca230c981b382c5990bd1a550ae3
blo2000 <pablo.hernandez.ortiz.12.8.2000@gmail.com>
2024-05-29 19:16:39 +0200

    Escalado terminado

--- app/src/main/java/com/example/escaladoypintura/Escalado.java
+++ app/src/main/java/com/example/escaladoypintura/Escalado.java
@@ -1,6 +1,8 @@
 package com.example.escaladoypintura;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Escalado extends AppCompatActivity {
@@ -26,7 +29,7 @@ public class Escalado extends AppCompatActivity {
         desplegable = (Spinner) findViewById(R.id.desplegable);
         datos = findViewById(R.id.datos);
 
-        // opciones del despelgable+
+        // opciones del despelgable
         ArrayList<String> opciones = new ArrayList<>();
         opciones.add("Escala");
         opciones.add("1/72");
@@ -41,44 +44,69 @@ public class Escalado extends AppCompatActivity {
         desplegable.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String op = (String) desplegable.getAdapter().getItem(position);
-                String datosStr = datos.getText().toString();
-                if (!datosStr.isEmpty()) {
-                    try {
-                        double dat = Double.parseDouble(datosStr);
-                        double resultadoEscala = 0;
-
-                        switch (op) {
-                            case "1/72":
-                                resultadoEscala = escalar(dat, 72);
-                                break;
-                            case "1/76":
-                                resultadoEscala = escalar(dat, 76);
-                                break;
-                            case "1/48":
-                                resultadoEscala = escalar(dat, 48);
-                                break;
-                            case "1/35":
-                                resultadoEscala = escalar(dat, 35);
-                                break;
-                            case "1/32":
-                                resultadoEscala = escalar(dat, 32);
-                                break;
-                        }
-
-                        resultado.setText(String.format(String.valueOf(resultadoEscala)));
-                    } catch (NumberFormatException e) {
-                        datos.setError("Numero no valido");
-                    }
-                }
+                actualizarResultado();
             }
+
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
+        datos.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                actualizarResultado();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+    private void actualizarResultado() {
+        String op = (String) desplegable.getSelectedItem();
+        String datosStr = datos.getText().toString();
+        if (!datosStr.isEmpty() && !op.equals("Escala")) {
+            try {
+                double dat = Double.parseDouble(datosStr);
+                double resultadoEscala = 0;
+                DecimalFormat df = new DecimalFormat("#.##");
+
+                switch (op) {
+                    case "1/72":
+                        resultadoEscala = escalar(dat, 72);
+                        break;
+                    case "1/76":
+                        resultadoEscala = escalar(dat, 76);
+                        break;
+                    case "1/48":
+                        resultadoEscala = escalar(dat, 48);
+                        break;
+                    case "1/35":
+                        resultadoEscala = escalar(dat, 35);
+                        break;
+                    case "1/32":
+                        resultadoEscala = escalar(dat, 32);
+                        break;
+                }
+
+                String resultadoFormateado = df.format(resultadoEscala);
+                resultado.setText(resultadoFormateado);
+            } catch (NumberFormatException e) {
+                datos.setError("Número no válido");
+            }
+        } else {
+            resultado.setText("");
+        }
+    }
+
     public static double escalar(double medida, double escala){
         return medida/escala;
     }
